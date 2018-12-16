1d2c9b084edc85d321a9341a7906f30cbbf392a7
Ruturaj Sawant <ruturajsawant5@gmail.com>
2018-12-16 21:04:26 +0530

    Updates

---
+++ precache-manifest.fa789c9cd4793a62645860b9832d814d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b88bd08916fdff0765",
+    "url": "/my-app/static/css/main.c64df46e.chunk.css"
+  },
+  {
+    "revision": "c2b88bd08916fdff0765",
+    "url": "/my-app/static/js/main.c2b88bd0.chunk.js"
+  },
+  {
+    "revision": "b2eabc8af688595ea911",
+    "url": "/my-app/static/js/1.b2eabc8a.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "10905006aad9c063321219b001925aea",
+    "url": "/my-app/index.html"
+  }
+];
